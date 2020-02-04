e0161ec9b9f3d90704900b9ab9edd353558b2e9f
Chloe Gell <gellert.chloe@gmail.com>
2020-02-04 21:11:59 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -9,7 +9,7 @@ class Doctor
     @@all 
   end 
   def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
+    Appointment.new(date, patient, self)
   end 
   def appointments 
     Appointment.all.select do |appt|
