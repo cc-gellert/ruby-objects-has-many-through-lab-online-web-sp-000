a7703c664d968c96dd7c03bd4baa738d3eacaf08
Chloe Gell <gellert.chloe@gmail.com>
2020-02-04 20:52:10 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist 
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  @@all = [] 
+  def self.all 
+    @@all 
+  end 
+  def new_song(name, genre)
+    made_song = Song.new(name, genre)
+    made_song.artist = self 
+  end 
+  def songs 
+    Song.all.select  do |song|
+      song.artist = self 
+    end 
+  end 
+end 
