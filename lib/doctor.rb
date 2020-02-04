20f3d1fa2fb1d95c6a59748d4cf10d78d1b564dc
Chloe Gell <gellert.chloe@gmail.com>
2020-02-04 21:06:17 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,2 +1,28 @@
 class Doctor 
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  @@all = [] 
+  def self.all 
+    @@all 
+  end 
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  end 
+  def appointments 
+    Appointment.all.select do |appt|
+      appt.doctor == self 
+    end 
+  end 
+  def patients 
+    patients = [] 
+    appointments.all.each do |appt|
+      if !(patients.include?(appt.patient))
+        patients << appt.patiend 
+      end 
+    end 
+    patients 
+  end 
 end 
