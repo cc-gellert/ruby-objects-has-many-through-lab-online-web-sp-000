ebbd3b20e6558525b5433a39277957a2795625df
Chloe Gell <gellert.chloe@gmail.com>
2020-02-04 21:11:28 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,12 @@
-class Appointment 
+class Appointment
+  attr_accessor :date, :patient, :doctor
+  @@all = [] 
+  def initialize(date, patient, doctor)
+    @patient = patient 
+    @doctor = doctor 
+    @@all << self 
+  end 
+  def self.all 
+    @@all 
+  end 
 end 
